c09240ed3e570059b404f6c9dd35e81617148caf
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-03-15 13:43:00 -0600

    FactoryMethod

---
+++ FactoryMethod.php
@@ -0,0 +1,45 @@
+<?php
+
+// Interface 
+
+interface level{
+    public function createCharacter();
+}
+ 
+
+// concrete Classes
+
+class skeleton implements level{
+    public function createCharacter(){
+        return "Skeleton Atacks". PHP_EOL;
+    }
+    
+}
+
+class zombie implements level{
+    public function createCharacter(){
+        return "Zombie bites". PHP_EOL;
+    }
+
+}
+
+// Factory Class
+class characterFactory{
+    public static function selectedLevel($level){
+        switch($level){
+            case 'easy':
+                return new skeleton();
+            case 'hard':
+                return new zombie();
+            default:
+            throw new Exception("Invalid level, select either Easy or Hard");
+        }
+    }
+}
+
+// Instatiations
+$user1 = characterFactory::selectedLevel('hard');
+echo $user1->createCharacter();
+
+$user2 = characterFactory::selectedLevel('easy');
+echo $user2->createCharacter();

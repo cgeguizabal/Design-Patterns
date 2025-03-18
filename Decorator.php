dd7f35e32b295aa97eec2bee599d5c79880ec193
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-03-17 21:30:44 -0600

    Decorator

--- Decorator.php
+++ Decorator.php
@@ -1,12 +1,3 @@
-/*
-Crear un programa donde sea posible añadir diferentes armas a ciertos personajes de videojuegos.
-Debes utilizar al menos dos personajes para este ejercicio.
-
-
-
-Para llevar a cabo esta tarea, aplica el patrón de diseño Decorator. */
-
-
 <?php
 
 interface ICharacter{
